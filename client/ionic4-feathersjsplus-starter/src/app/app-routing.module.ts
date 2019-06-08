0879620f9d792cec7da82f9d9fd4f1bd6c96c0e0
iva2k <iva2k@yahoo.com>
2019-06-08 00:59:59 -0700

    Step 6 add guards to routes

--- client/ionic4-feathersjsplus-starter/src/app/app-routing.module.ts
+++ client/ionic4-feathersjsplus-starter/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuardService } from './services/auth-guard.service';
+import { NonauthGuardService } from './services/nonauth-guard.service';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'menu/app/tabs/home', pathMatch: 'full' },
-  { path: 'menu', loadChildren: './pages/menu/menu.module#MenuPageModule' },
-  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+// ?  { path: '', redirectTo: 'login', pathMatch: 'full', canActivate: [NonauthGuardService] },
+// ?  { path: '', redirectTo: 'menu', pathMatch: 'full', canActivate: [AuthGuardService] },
+  // TODO: (soon) try canLoad instead of canActivate
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule', canActivate: [NonauthGuardService] },
+  { path: 'menu' , loadChildren: './pages/menu/menu.module#MenuPageModule'   , canActivate: [AuthGuardService   ] },
 ];
 
 @NgModule({
