73cafa43e5446d77b1436811b8937351e476ae8e
iva2k <iva2k@yahoo.com>
2019-06-19 23:11:15 -0700

    Step 10 password show/hide toggle button color

--- client/ionic4-feathersjsplus-starter/src/app/directives/show-hide-pass.directive.ts
+++ client/ionic4-feathersjsplus-starter/src/app/directives/show-hide-pass.directive.ts
@@ -1,18 +1,38 @@
-import {Directive, ElementRef, HostListener, Input} from '@angular/core';
+import {Directive, ElementRef, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
+import { DomController } from '@ionic/angular';
 
+/**
+ * Implements a directive that ties a button (placed anywhere) to toggling password field between hide and show.
+ * Also toggles class on the button between toggle-text and toggle-password, so it can be styled like a toggle.
+ */
+const classPrefix = 'toggle-';
 @Directive({
   selector: '[appShowHidePass]'
 })
-export class ShowHidePassDirective {
+export class ShowHidePassDirective implements OnInit {
   @Input('appShowHidePass') targetInput: any;
 
-  constructor(private el: ElementRef) {}
+  constructor(
+    private el: ElementRef,
+    private renderer: Renderer2,
+    private domCtrl: DomController
+  ) {}
+
+  ngOnInit() {
+    this.domCtrl.write(() => {
+      this.renderer.addClass(this.el.nativeElement, classPrefix + this.targetInput.type);
+    });
+  }
 
   @HostListener('click') onClick() {
     // console.log('DEBUG directive onClick, el: %o, targetInput: %o', this.el, this.targetInput);
     const newType = (this.targetInput.type === 'text') ? 'password' : 'text';
-    this.targetInput.type = newType;
-    // TODO: (later) implement toggling class on the button this.el, so it can be styled
+    // Efficient way to write to DOM:
+    this.domCtrl.write(() => {
+      this.renderer.removeClass(this.el.nativeElement, classPrefix + this.targetInput.type);
+      this.targetInput.type = newType;
+      this.renderer.addClass(this.el.nativeElement, classPrefix + this.targetInput.type);
+    });
   }
 
 }
