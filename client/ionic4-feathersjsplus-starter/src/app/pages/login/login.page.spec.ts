a0ef9623cd7d304c7844c79190c8e399336ed0a6
iva2k <iva2k@yahoo.com>
2019-08-23 16:22:01 -0700

    fix login.page.spec

--- client/ionic4-feathersjsplus-starter/src/app/pages/login/login.page.spec.ts
+++ client/ionic4-feathersjsplus-starter/src/app/pages/login/login.page.spec.ts
@@ -1,16 +1,62 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Injectable, Component } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IonSlides, NavController, Platform } from '@ionic/angular';
 
 import { LoginPage } from './login.page';
+
+import { FeathersService, Login } from '../../services/feathers.service';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+@Injectable({
+  providedIn: 'root'
+})
+class MockFeathersService {
+  private socialLogins: Promise<Login[]> = Promise.resolve([]);
+  public getSocialLogins(): Promise<any[]> {
+    return this.socialLogins;
+  }
+}
+
+@Component({
+  selector: 'ion-slides',
+  template: '',
+  providers: [
+    { provide: IonSlides, useClass: StubIonSlidesComponent },
+  ]
+})
+class StubIonSlidesComponent {
+  public slideTo(index: number, speed: number) {}
+}
 
 describe('LoginPage', () => {
   let component: LoginPage;
   let fixture: ComponentFixture<LoginPage>;
+  let platformReadySpy, platformIsSpy, platformSpy;
 
   beforeEach(async(() => {
+    platformReadySpy = Promise.resolve();
+    platformIsSpy = (query: string) => query === 'cordova';
+    platformSpy = jasmine.createSpyObj('Platform', { ready: platformReadySpy, is: platformIsSpy });
+
     TestBed.configureTestingModule({
-      declarations: [ LoginPage ],
+      declarations: [
+        LoginPage,
+        StubIonSlidesComponent, // IonSlides
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [
+        RouterTestingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        NoopAnimationsModule,
+      ],
+      providers: [
+        { provide: NavController, useValue: null }, // NavController,
+        { provide: Platform, useValue: platformSpy },
+        { provide: FeathersService, useClass: MockFeathersService },
+      ],
     })
     .compileComponents();
   }));
@@ -18,7 +64,7 @@ describe('LoginPage', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginPage);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    fixture.detectChanges(); // ngOnInit()??
   });
 
   it('should create', () => {
