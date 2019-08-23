427738662a8fef061ef3c1086cc9ee3bd38ba2ae
iva2k <iva2k@yahoo.com>
2019-08-23 14:46:11 -0700

    fix todos-list.component.spec

--- client/ionic4-feathersjsplus-starter/src/app/components/todos-list/todos-list.component.spec.ts
+++ client/ionic4-feathersjsplus-starter/src/app/components/todos-list/todos-list.component.spec.ts
@@ -1,7 +1,40 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Injectable } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TodosListComponent } from './todos-list.component';
+
+import { Record, DataSubscriber, FeathersService } from '../../services/feathers.service';
+
+// Mock data for the tests, presented through MockService.find()
+const mockData = [
+  {},
+  {},
+  {},
+];
+
+class MockService {
+  constructor(private name: string, private data: {}[]) {}
+  public on(event: string, cb: (record: any) => void) {}
+  public find(query: string) {
+    return Promise.resolve(this.data);
+  }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+class FeathersMockService {
+  public service(name: string, data: {}[]) {
+    return new MockService(name, data);
+  }
+  public subscribe<T extends Record>(service: string, query: any, cbData: (records: any) => void, cbErr: (err: any) => void): any {
+    const subscriber = new DataSubscriber<T>(this.service(service, mockData), cbData, cbErr);
+    subscriber.find(query)
+      .catch(err => { cbErr(err); })
+    ;
+    return subscriber;
+  }
+}
 
 describe('TodosListComponent', () => {
   let component: TodosListComponent;
@@ -11,6 +44,9 @@ describe('TodosListComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ TodosListComponent ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+         {provide: FeathersService, useClass: FeathersMockService},
+      ],
     })
     .compileComponents();
   }));
@@ -24,4 +60,7 @@ describe('TodosListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // TODO: (later) add more tests!
+
 });
