d60999a7d9c8c7324d2279c7727267c3fd25e18c
iva2k <iva2k@yahoo.com>
2019-08-23 23:47:29 -0700

    fix nonauth-guard.service.spec

--- client/ionic4-feathersjsplus-starter/src/app/services/nonauth-guard.service.spec.ts
+++ client/ionic4-feathersjsplus-starter/src/app/services/nonauth-guard.service.spec.ts
@@ -1,12 +1,35 @@
-import { TestBed } from '@angular/core/testing';
+import { Injectable } from '@angular/core';
+import { async, TestBed } from '@angular/core/testing';
+
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { NonauthGuardService } from './nonauth-guard.service';
 
+import { FeathersService } from '../services/feathers.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+class MockFeathersService {
+}
+
 describe('NonauthGuardService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+      ],
+      providers: [
+        { provide: FeathersService, useClass: MockFeathersService },
+      ],
+    });
+  }));
 
   it('should be created', () => {
     const service: NonauthGuardService = TestBed.get(NonauthGuardService);
     expect(service).toBeTruthy();
   });
+
+  // TODO: (later) add more tests!
+
 });
