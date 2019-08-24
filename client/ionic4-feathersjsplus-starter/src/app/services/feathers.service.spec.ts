fe7b2929308997527fe311e3135fb855fbc6e45e
iva2k <iva2k@yahoo.com>
2019-08-23 23:52:36 -0700

    fix feathers.service.spec

--- client/ionic4-feathersjsplus-starter/src/app/services/feathers.service.spec.ts
+++ client/ionic4-feathersjsplus-starter/src/app/services/feathers.service.spec.ts
@@ -1,12 +1,28 @@
-import { TestBed } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 
+import { RouterTestingModule } from '@angular/router/testing';
 import { FeathersService } from './feathers.service';
+import { HttpClientModule } from '@angular/common/http';
+import { GooglePlus } from '@ionic-native/google-plus/ngx';
 
 describe('FeathersService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientModule,
+      ],
+      providers: [
+        GooglePlus,
+      ],
+    });
+  }));
 
   it('should be created', () => {
     const service: FeathersService = TestBed.get(FeathersService);
     expect(service).toBeTruthy();
   });
+
+  // TODO: (later) add more tests!
+
 });
