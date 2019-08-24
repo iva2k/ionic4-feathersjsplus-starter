03dcea1a91b702a255b2e339a777dad780c73f9e
iva2k <iva2k@yahoo.com>
2019-08-23 23:47:11 -0700

    fix auth-guard.service.spec

--- client/ionic4-feathersjsplus-starter/src/app/services/auth-guard.service.spec.ts
+++ client/ionic4-feathersjsplus-starter/src/app/services/auth-guard.service.spec.ts
@@ -1,12 +1,35 @@
-import { TestBed } from '@angular/core/testing';
+import { Injectable } from '@angular/core';
+import { async, TestBed } from '@angular/core/testing';
+
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AuthGuardService } from './auth-guard.service';
 
+import { FeathersService } from '../services/feathers.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+class MockFeathersService {
+}
+
 describe('AuthGuardService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+      ],
+      providers: [
+        { provide: FeathersService, useClass: MockFeathersService },
+      ],
+    });
+  }));
 
   it('should be created', () => {
     const service: AuthGuardService = TestBed.get(AuthGuardService);
     expect(service).toBeTruthy();
   });
+
+  // TODO: (later) add more tests!
+
 });
