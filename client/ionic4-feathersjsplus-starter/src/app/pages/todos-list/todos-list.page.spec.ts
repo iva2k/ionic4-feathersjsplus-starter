ea750cf9e79a05894ff10519687fb60bea5675b8
iva2k <iva2k@yahoo.com>
2019-08-23 23:35:10 -0700

    fix todos-list.page.spec

--- client/ionic4-feathersjsplus-starter/src/app/pages/todos-list/todos-list.page.spec.ts
+++ client/ionic4-feathersjsplus-starter/src/app/pages/todos-list/todos-list.page.spec.ts
@@ -1,7 +1,16 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Injectable } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NavController } from '@ionic/angular';
 
 import { TodosListPage } from './todos-list.page';
+
+import { FeathersService } from '../../services/feathers.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+class MockFeathersService {
+}
 
 describe('TodosListPage', () => {
   let component: TodosListPage;
@@ -9,16 +18,22 @@ describe('TodosListPage', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TodosListPage ],
+      declarations: [
+        TodosListPage
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        { provide: NavController, useValue: null }, // NavController,
+        { provide: FeathersService, useClass: MockFeathersService },
+      ],
     })
-      .compileComponents();
+    .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TodosListPage);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    fixture.detectChanges(); // ngOnInit()
   });
 
   it('should create', () => {
