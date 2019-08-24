a3ef6ee9159b1a330001edc43ade248d0b16f99e
iva2k <iva2k@yahoo.com>
2019-08-23 23:15:53 -0700

    fix reset-password.page.spec

--- client/ionic4-feathersjsplus-starter/src/app/pages/reset-password/reset-password.page.spec.ts
+++ client/ionic4-feathersjsplus-starter/src/app/pages/reset-password/reset-password.page.spec.ts
@@ -1,7 +1,21 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Injectable } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { ResetPasswordPage } from './reset-password.page';
+
+import { FeathersService } from '../../services/feathers.service';
+import { User } from '../../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+class MockFeathersService {
+  private user = {};
+  public authenticate(credentials?): Promise<any|User> { return Promise.resolve(this.user); }
+  public resetPassword(resetToken, credentials): Promise<any> { return Promise.resolve(this.user); }
+}
 
 describe('ResetPasswordPage', () => {
   let component: ResetPasswordPage;
@@ -9,8 +23,18 @@ describe('ResetPasswordPage', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ResetPasswordPage ],
+      declarations: [
+        ResetPasswordPage,
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [
+        RouterTestingModule,
+        FormsModule,
+        ReactiveFormsModule,
+      ],
+      providers: [
+        { provide: FeathersService, useClass: MockFeathersService },
+      ],
     })
     .compileComponents();
   }));
@@ -18,7 +42,7 @@ describe('ResetPasswordPage', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ResetPasswordPage);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    fixture.detectChanges(); // ngOnInit()
   });
 
   it('should create', () => {
