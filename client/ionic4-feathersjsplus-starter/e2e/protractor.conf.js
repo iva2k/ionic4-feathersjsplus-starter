82bb0a51cc8caaaaa5e24d6ff12d3ee28e27d076
iva2k <iva2k@yahoo.com>
2019-08-30 12:59:08 -0700

    fix e2e

--- client/ionic4-feathersjsplus-starter/e2e/protractor.conf.js
+++ client/ionic4-feathersjsplus-starter/e2e/protractor.conf.js
@@ -4,7 +4,7 @@
 const { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
-  allScriptsTimeout: 11000,
+  allScriptsTimeout: 20000,
   specs: [
     './src/**/*.e2e-spec.ts'
   ],
